4c9ec088447497c1502b20fc98e777e2b02d675a
Wikpi <66695611+Wikpi@users.noreply.github.com>
2025-07-10 10:56:14 +0200

    Refactor plugin logic (#13)
    This commit fixes raised issues with the plugin logic workings:
     - Root node plugin effect history will be saved over engine
       restarts.
     - Plugin cleans up any leftovers either on disable or a restart.
     - Optimized plugin logic to let each merged sprite custom node
       handle their own processing.
    - Remove redundant code.

--- addons/group_shader/PluginMergedSprite.gd
+++ addons/group_shader/PluginMergedSprite.gd
@@ -5,13 +5,31 @@ onready var viewport: Viewport = $Viewport
 onready var merged_sprite: Sprite = $Sprite
 onready var viewport_sprites: Node2D = $Viewport/Sprites
 
+var plugin_tools = preload("res://addons/group_shader/plugin_tools.gd")
+
+var root_node: Node2D
+var signature: String
+
 func _ready():
 	viewport.size = Vector2(64, 64)
 	viewport.transparent_bg = true
 	viewport.render_target_v_flip = true
 	viewport.render_target_update_mode = Viewport.UPDATE_ALWAYS
 
-func merge_sprites(sprites: Array, bounds: Dictionary) -> void:
+func _process(delta):
+	if !is_instance_valid(root_node):
+		return
+
+	var sprites = plugin_tools.get_sprites(root_node)
+	var new_signature = plugin_tools.generate_node_signature(sprites)
+
+	if signature == new_signature:
+		return
+	
+	var bounds = plugin_tools.calculate_sprite_bounds(sprites)
+	
+	modify_sprite_material(root_node.material.duplicate())
+	
 	modify_viewport_bounds(bounds.size, -bounds.min)
 
 	modify_viewport_sprite_container(sprites)
@@ -43,16 +61,16 @@ func modify_viewport_sprite_container(new_sprites: Array) -> void:
 		viewport_sprites.add_child(clone_sprite(sprite))
 
 func clone_sprite(new_sprite: Sprite) -> Sprite:
-		var clone := Sprite.new()
+	var clone := Sprite.new()
 
-		clone.texture = new_sprite.texture
-		clone.scale = new_sprite.scale
-		clone.centered = new_sprite.centered
-		clone.rotation = new_sprite.rotation
-		clone.modulate = new_sprite.modulate
-		clone.region_enabled = new_sprite.region_enabled
-		clone.region_rect = new_sprite.region_rect
+	clone.texture = new_sprite.texture
+	clone.scale = new_sprite.scale
+	clone.centered = new_sprite.centered
+	clone.rotation = new_sprite.rotation
+	clone.modulate = new_sprite.modulate
+	clone.region_enabled = new_sprite.region_enabled
+	clone.region_rect = new_sprite.region_rect
 
-		clone.transform = new_sprite.get_global_transform()
-		
-		return clone
+	clone.transform = new_sprite.get_global_transform()
+	
+	return clone
